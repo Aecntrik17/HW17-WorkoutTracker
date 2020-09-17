4925b9aec11c106fe2a3bfa64f5722c7ca966fcf
Aaron Sims <aaron@adsimsllc.com>
2020-09-17 13:35:34 -0400

    updating dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const apiRoutes = require("./routes/api-routes");
 const htmlRoutes = require("./routes/html-routes");
