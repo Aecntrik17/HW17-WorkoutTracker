2ed52a06ba9d8d132a3abdff85f5cd8f3119d7bf
Aaron Sims <aaron@adsimsllc.com>
2020-09-16 20:35:22 -0400

    working on heroku html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const Router = express.Router();
 const path = require("path");
 
-Router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 Router.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 Router.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+Router.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = Router;
